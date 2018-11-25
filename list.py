14ca4c47cc5e3fcfbe6d5cfe178b128ad74ac590
OXFORDX <42910683+OXFORDX@users.noreply.github.com>
2018-11-25 16:50:18 +0200

    lab7.py
    Задані масив В(n) дійсних чисел і натуральне z. Визначити кількість елементів масиву B, менших за z. Знайти максимальний із цих елементів і його індекс. Поміняти місцями перший і знайдений максимальний елемент.
    @nechaido review Володя Орлов ІП-82

---
+++ list.py
@@ -0,0 +1,33 @@
+import random
+z = int(input("Введіть число від 1 до 20:"))
+rlist = []
+
+for i in range(0, 20):
+    x = random.randint(0, 20)
+    rlist.append(x)
+
+print(rlist)
+k = 0
+listz = []
+for i in range(0, 20):
+    if rlist[k] < z:
+        listz.append(rlist[k])
+    k += 1
+print("Масив елементів < z =", listz)
+print('Його довжина:', len(listz))
+x = max(listz)
+print('Його максимальний елемент:', x)
+l = int(len(listz))
+n = 0
+for i in range(0, l):
+    if listz[i] == x:
+        print('Його індекс:', listz.index(x))
+        listz[n], listz[i] = listz[i], listz[n]
+        h = int(listz[i])
+        n = int(listz[n])
+        break
+for i in range(n, l):
+    if listz[i] == x:
+        listz[i] = h
+print('Фінальний массив виконаний зі всіма умовами =', listz)
+input('press ENTER to exit')
