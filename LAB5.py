091f7a9e7bb0716380913daadfefc27fc036c18e
OXFORDX <42910683+OXFORDX@users.noreply.github.com>
2018-11-29 19:43:26 +0200

    Update LAB5.py
    @nechaido Update lab5

--- LAB5.py
+++ LAB5.py
@@ -1,11 +1,8 @@
-a = int(input('Введть натуральне число:'))
-k = ''
-while a > 0:
-    y = str(a % 2)
-    k += y
-    a = int(a / 2)
-x = k[::-1]
-print(x)
-print(k)
-print(int(k, 2))
-input('press ENTER to exit')
+n1 = int(input('N:'))
+n2 = 0
+while n1 > 0:
+    if n2 != 0:
+        n2 = int(n2) << 1
+    n2 += int(n1 % 2)
+    n1 = n1 >> 1
+print(n2)
